59e397d66d0949eb9f4f6b376b5ffe76381eb45c
Monica Gerard <mlgvla@gmail.com>
2022-11-13 10:31:40 -0500

    solution

---
+++ app/migrations/versions/eac5040f92ec_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: eac5040f92ec
+Revises: 
+Create Date: 2022-11-12 23:42:33.374084
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eac5040f92ec'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
